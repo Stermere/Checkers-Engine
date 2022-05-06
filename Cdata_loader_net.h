56ba2f926e5ee70de108f9a1be559aaf4984dd1c
Stermere <collin@kees.net>
2022-05-06 11:00:55 -0700

    added files for the NN and basic skeleton

---
+++ Cdata_loader_net.h
@@ -0,0 +1,40 @@
+// provides the functions to load save and create data sets from the python list input of games, the predicted value at that location
+// and the end game state of the game. allowing for the creation of a data set for training and testing.
+// and to create the data set for the neural network
+
+#include <Python.h>
+
+// struct to hold the data set
+struct data_set{
+
+};
+
+// function to load the data set from the python list input
+void load_data_set_from_py(){
+
+}
+
+// function to save the data set to a file
+void save_data_set_to_file(){
+
+}
+
+// function to load the data set from a file
+void load_data_set_from_file(){
+
+}
+
+// function to load the neural network from a file
+void load_neural_network_from_file(){
+
+}
+
+// funciton to save the neural network to a file
+void save_neural_network_to_file(){
+
+}
+
+// function to calculate the actual values of the data set
+void calculate_actual_values(){
+
+}
---
+++ Cneural_net.h
@@ -0,0 +1,24 @@
+// neural network for evaluating checkers moves
+// file contains the structs and functions for the neural network
+
+
+// structure for the neural network
+struct neural_net {
+};
+
+// structure for a layer of the neural network
+struct layer {
+};
+
+// structure for a neuron of the neural network
+struct neuron {
+};
+
+// get the output of the neural network for a given input
+double get_output(struct neural_net *net, long long int p1, long long int p2, long long int p1k, long long int p2k){
+    return 0;
+}
+
+// train the neural network
+void train(struct neural_net *net, double target){
+}
