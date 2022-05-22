a9fb42e11f0792b00fedded179e165fe4fb387f6
Collin Kees <47161917+Stermere@users.noreply.github.com>
2022-05-22 15:26:56 -0700

    network fixes

--- Cneural_net.h
+++ Cneural_net.h
@@ -8,7 +8,7 @@
 
 
 // declerations
-void activation(struct neuron *n, struct layer *last_layer);
+void activation(struct neuron *n, struct layer *last_layer, double squish_val);
 void populate_input(struct neural_net *net, long long int p1, long long int p2, long long int p1k, long long int p2k);
 void forward_propagate(struct neural_net *net);
 double transfer_tanh(double activation);
@@ -36,36 +36,40 @@ double get_output(struct neural_net *net, long long int p1, long long int p2, lo
 // put the input in to the first layers output
 void populate_input(struct neural_net *net, long long int p1, long long int p2, long long int p1k, long long int p2k){
     struct layer *input_layer = net->layers;
-    int v1 = 0;
-    int shift_amount = 1;
-    int num_loops = net->num_layers;
+    int shift_map[] = {
+        1, 3, 5, 7,
+        8, 10, 12, 14,
+        17, 19, 21, 23,
+        24, 26, 28, 30,
+        33, 35, 37, 39,
+        40, 42, 44, 46,
+        49, 51, 53, 55,
+        56, 58, 60, 62,
+    };
+    int shift_amount;
+    int num_loops = input_layer->num_neurons;
     double input_value;
     for (int i = 0; i < num_loops; i++){
+        shift_amount = shift_map[i];
         if (p1 >> shift_amount & 1 == 1){
-            input_value = 1.0;
-        }
-        else if (p2 >> shift_amount & 1 == 1){
-            input_value = -1.0;
-        }
-        else if (p1k >> shift_amount & 1 == 1){
             input_value = 3.0;
         }
+        else if (p2 >> shift_amount & 1 == 1){
+            input_value = 2.0;
+        }
+        else if (p1k >> shift_amount & 1 == 1){
+            input_value = 4.0;
+        }
         else if (p2k >> shift_amount & 1 == 1){
-            input_value = -3.0;
+            input_value = 1.0;
         }
         else{
             input_value = 0.0;
         }
 
-        input_layer->neurons[v1].output = input_value;
-        shift_amount += 2;
-        v1++;
+        input_layer->neurons[i].output = input_value;
     }
 }
-
-
-///////////////////////////////////////////////////////////////
-// begining of more generalized code
 
 
 // back propogate the error
@@ -84,6 +88,7 @@ void back_propagate(struct neural_net *net, double learning_rate, double *target
                 // TODO this can be optimized
                 error += error_relu_hidden(output_layer->neurons[k].weights[j], output_layer->neurons[k].error, current_layer->neurons[j].output);
             }
+            error = error / current_layer->num_neurons;
             current_layer->neurons[j].error = error;
         }
         current_layer--;
@@ -116,12 +121,15 @@ void forward_propagate(struct neural_net *net){
     struct layer* current_layer = net->layers + 1;
     int current_layer_index = 1;
     int num_layers = net->num_layers - 1;
-    // loop through the layers
+
+     // loop through the layers
     for (int i = 1; i < num_layers; i++){
         // loop throught the neurons in the layer
-        int num_neurons = net->layers[i].num_neurons;
+        // also compute 1/total_weights
+        int num_neurons = last_layer->num_neurons;
+        double squish_val = 1.0 / (double)num_neurons;
         for (int j = 0; j < num_neurons; j++){
-            activation(current_layer->neurons + j, last_layer);
+            activation(current_layer->neurons + j, last_layer, squish_val);
             current_layer->neurons[j].output = transfer_relu(current_layer->neurons[j].output);
             
         }
@@ -129,16 +137,14 @@ void forward_propagate(struct neural_net *net){
         last_layer = current_layer;
         current_layer = net->layers + current_layer_index;
     }
-    // last layer is the output layer
-    // so put it through the tanh function
-    int num_neurons = net->layers[num_layers].num_neurons;
+    // now activate the last layer
+    int num_neurons = net->layers[net->num_layers - 1].num_neurons;
+    double squish_val = 1.0 / (double)num_neurons;
     for (int i = 0; i < num_neurons; i++){
-        activation(current_layer->neurons + i, last_layer);
-        printf("outptut: %f\n", current_layer->neurons[i].output);
+        activation(current_layer->neurons + i, last_layer, squish_val);
         current_layer->neurons[i].output = transfer_relu(current_layer->neurons[i].output);
     }
 }
-
 
 // transforms the value using the tanh function
 double transfer_tanh(double activation){
@@ -163,10 +169,10 @@ double error_tanh_hidden(double weight, double error, double activation){
 
 // transform the value using the ReLU function
 double transfer_relu(double activation){
-    if (activation > 0.0){
+    if (activation >= 0.0){
         return activation;
     }
-    return 0.0;
+    return activation * 0.01;
 }
 
 // returns the derivative of the ReLU function
@@ -174,7 +180,7 @@ double transfer_relu_deriv(double activation){
     if (activation >= 0.0){
         return 1.0;
     }
-    return 0.0;
+    return 0.01;
 }
 
 // return the error of the output
@@ -188,7 +194,7 @@ double error_relu_hidden(double weight, double error, double activation){
 }
 
 // activate a neuron
-void activation(struct neuron *n, struct layer *last_layer){
+void activation(struct neuron *n, struct layer *last_layer, double squish_val){
     double sum1 = 0;
     double sum2 = 0;
     double sum3 = 0;
@@ -196,7 +202,7 @@ void activation(struct neuron *n, struct layer *last_layer){
     int num_weights = n->prev_layer_neurons_num;
     int final_loop = num_weights % 4;
     //sum the weights and the inputs
-    for (int i = 0; i < num_weights; i = i + 4){
+    for (int i = 0; i < num_weights; i += 4){
         sum1 += n->weights[i] * last_layer->neurons[i].output;
         sum2 += n->weights[i + 1] * last_layer->neurons[i + 1].output;
         sum3 += n->weights[i + 2] * last_layer->neurons[i + 2].output;
@@ -213,5 +219,5 @@ void activation(struct neuron *n, struct layer *last_layer){
     sum1 = sum1 + sum3;
  
     //set the output
-    n->output = sum1;
+    n->output = sum1 * squish_val;
 }
