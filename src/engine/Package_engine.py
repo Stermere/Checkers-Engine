5047acdaafc34637a6fbdba8f7139e91b8b4a051
Collin Kees <Collin@kees.net>
2023-07-10 19:21:12 -0700

    temperarly fix setup.py

--- src/engine/Package_engine.py
+++ src/engine/Package_engine.py
@@ -1,12 +1,14 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 def main():
-    setup(name="search_engine",
-          version="1.0.0",
-          description="Python interface for searching a given checkers board",
-          author="Collin Kees",
-          author_email="Collin@kees.net",
-          ext_modules=[Extension("search_engine", ["src/engine/board_search.c"])])
+    setup(
+        name="search_engine",
+        version="1.0.0",
+        description="Python interface for searching a given checkers board",
+        author="Collin Kees",
+        author_email="Collin@kees.net",
+        ext_modules=[Extension("search_engine", ["src/engine/board_search.c"])]
+    )
 
 if __name__ == "__main__":
     main()
