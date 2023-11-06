a3585dac07e8f57ac0af789b9f145bf4d697cbfc
Collin Kees <Collin@kees.net>
2023-11-05 17:48:57 -0800

    add view of structures being checked in the evaluation function

--- src/python/convert_board_to_bit.py
+++ src/python/convert_board_to_bit.py
@@ -1,52 +1,40 @@
 import Board_opperations as bo
 import bitboard_converter as bc
 
-# structures that are already included:
-#board = [[0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0],   
-#         [0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 1, 0, 0, 0, 0],
-#         [0, 0, 1, 0, 1, 0, 0, 0]]
+# This file serves as a refrence and helper for the board evaluation bit check evals.
+# Positions below are the positions being given a bonus
 
-#board = [[0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0],   
-#         [0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 1, 0, 1],
-#         [0, 0, 0, 0, 0, 0, 1, 0]]
+# first check
+"""[[0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 2, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]]
+"""
 
-#board = [[0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0], 
-#         [0, 0, 0, 0, 0, 0, 0, 0],
-#         [1, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 1, 0, 0, 0, 0, 0, 0],
-#         [1, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0]]
+# second check
+"""[[0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 2, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]]
+"""
 
 board = [[0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0], 
          [0, 0, 0, 0, 0, 0, 0, 0],
+         [1, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 1, 0, 0, 0, 0, 0, 0],
-         [1, 0, 1, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0]]
-
-#board = [[0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0], 
-#         [0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 1, 0, 0, 0, 1, 0]]
-
+         
 
 # convert to a bitboard
 p1, p2, p1k, p2k = bc.convert_to_bitboard(board)
@@ -57,14 +45,27 @@ print(hex(p2))
 print(hex(p1k))
 print(hex(p2k))
 print()
-print("board in bit format reversed:")
+
 # reverse the board
 for item in board:
     item.reverse()
 board.reverse()
 
+# reverse the players
+for item in board:
+    for i in range(len(item)):
+        if item[i] == 1:
+            item[i] = 2
+        elif item[i] == 2:
+            item[i] = 1
+        elif item[i] == 3:
+            item[i] = 4
+        elif item[i] == 4:
+            item[i] = 3
+
 # convert to a bitboard
 p1, p2, p1k, p2k = bc.convert_to_bitboard(board)
+print("check for other player:")
 print(hex(p1))
 print(hex(p2))
 print(hex(p1k))
